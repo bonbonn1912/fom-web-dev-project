84fab74dd0a2731391c1f6914f612d69ae156d2d
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-10-22 11:14:05 +0200

    Update vite config for pwa shortcuts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,55 +1,76 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [react(),VitePWA({
-    workbox: {
-      runtimeCaching:[
-        {
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        runtimeCaching: [
+          {
             urlPattern: /^\/logout/,
-            handler: 'NetworkOnly',
-        },
-        {
-          urlPattern: /^\/auth\//,
-          handler: 'NetworkOnly', // oder ein anderer Handler, je nach Anforderung
-        },
-      ],
-      navigateFallbackDenylist: [/^\/auth\//,/^\/logout/],
-    },
-    manifest:{
-      start_url: "/",
-      scope: "/",
-      display: "standalone",
-      icons:[{
-        src: "bicycle.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable"
-      }]
-    }
-  })],
+            handler: "NetworkOnly",
+          },
+          {
+            urlPattern: /^\/auth\//,
+            handler: "NetworkOnly", // oder ein anderer Handler, je nach Anforderung
+          },
+        ],
+        navigateFallbackDenylist: [/^\/auth\//, /^\/logout/],
+      },
+      manifest: {
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        shortcuts: [
+          {
+            name: "Dashboard",
+            short_name: "DB",
+            description: "Your Workout Dashboard",
+            url: "/dashboard/workouts",
+            icons: [
+              {
+                src: "bicycle.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any maskable",
+              },
+            ],
+          },
+        ],
+        icons: [
+          {
+            src: "bicycle.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
-    outDir: '../build/client/'
+    outDir: "../build/client/",
   },
   server: {
     port: 3001,
     strictPort: true,
     proxy: {
-      '/auth': {
-        target: 'http://localhost:5000',
+      "/auth": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
-      '/logout': {
-        target: 'http://localhost:5000',
+      "/logout": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
