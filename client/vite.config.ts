549263a68499f8061d1e1de18f4459a65290eeed
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-10-22 15:16:47 +0200

    changed deploy script

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  /*plugins: [
+  plugins: [
     react(),
     VitePWA({
       workbox: {
@@ -48,8 +48,8 @@ export default defineConfig({
         ],
       },
     }),
-  ], */
-  plugins: [react()],
+  ],
+ // plugins: [react()],
   build: {
     outDir: "../build/client/",
   },
