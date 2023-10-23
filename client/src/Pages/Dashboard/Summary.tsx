16431b5526455309100f1fdd1a67a9b44c1b1529
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-10-23 14:58:49 +0200

    fixed NaN value for avg speed

--- client/src/Pages/Dashboard/Summary.tsx
+++ client/src/Pages/Dashboard/Summary.tsx
@@ -8,11 +8,18 @@ interface MySummaryProps {
     totalTime: number;
 }
 const MySummary = ({totalDistance, totalCalories, totalTime}: MySummaryProps) => {
+    let avgSpeed = 0;
+    if(totalDistance === 0 || totalTime === 0){
+        avgSpeed = 0;
+    }else{
+        //@ts-ignore
+        avgSpeed = ((totalDistance/1000)/(totalTime/3600)).toFixed(2);
+    }
     const [stats] = useState([
         { name: 'Total Distance', stat: `${(totalDistance/1000).toFixed(2)} Km`, icon: <GiPathDistance size={20}/> },
         { name: 'Total Calories', stat: totalCalories, icon: <SlEnergy size={20}/> },
         { name: 'Total Time spent', stat: `${(totalTime/3600).toFixed(1)} h`, icon: <SlClock size={20}/> },
-        { name: 'Average Speed', stat: `${((totalDistance/1000)/(totalTime/3600)).toFixed(2)} Km/h`, icon: <SlSpeedometer size={20}/> },
+        { name: 'Average Speed', stat: `${avgSpeed} Km/h`, icon: <SlSpeedometer size={20}/> },
 
     ])
 
