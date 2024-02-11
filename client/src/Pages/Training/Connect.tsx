41f3a222d00fec71e68f16e9c8cdee9f4e1ef97b
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2024-02-11 11:33:54 +0100

    removed import for working build

--- client/src/Pages/Training/Connect.tsx
+++ client/src/Pages/Training/Connect.tsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from "react";
 import {GiHeartBeats} from "react-icons/gi";
 import {SiSmartthings} from "react-icons/si";
-import {useTranslation} from "react-i18next";
 
 interface IConnectProps {
     deviceHandler: (device: Map<string, BluetoothDevice> | null, characteristic: Map<string, BluetoothRemoteGATTCharacteristic> | null) => void;
